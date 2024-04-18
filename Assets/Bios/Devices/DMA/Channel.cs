c81d0426948a1b51df9e179c181d72eae09b3ab7
Rodrigo Estape de Oliveira <rodrigo_estape@hotmail.com>
2024-04-18 08:40:04 -0300

    Revert "Removed unnecessary packages"
    This reverts commit 99100335e9ee4e0d0d2e14b958b5360efa061604.

---
+++ Assets/Bios/Devices/DMA/Channel.cs
@@ -0,0 +1,8 @@
+namespace ProjectPSX.Devices
+{
+    public abstract class Channel
+    {
+        public abstract void write(uint register, uint value);
+        public abstract uint load(uint regiter);
+    }
+}
